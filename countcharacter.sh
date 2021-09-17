ad14addf97a67624b4bb097229b37b5a4205d68a
Ubuntu <ubuntu@ip-172-31-25-241.us-east-2.compute.internal>
2021-09-17 07:08:22 +0000

    added two files

---
+++ countcharacter.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo"enter the name of a file"
+read name
+num=1
+while read line
+do
+	n=`echo "$line" | wc -c`
+	echo "the number of characters in line number $num is $n"
+	num=`expr$num + 1`
+done <$name
+
